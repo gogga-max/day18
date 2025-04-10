ee85840767346bc5554a303744fbbcdf58631caf
Gfaddd <gfaddde@gmail.com>
2025-04-10 09:11:10 +0300

    Добавьте файлы проекта.

---
+++ Hotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day18
+{
+	internal class Hotel
+	{
+		public int id { get; set; }
+		public string Name { get; set; }
+		public int CountOfStars { get; set; }
+		public string CountryName { get; set; }
+		public string HotelImage { get; set; }
+
+
+
+	}
+}
